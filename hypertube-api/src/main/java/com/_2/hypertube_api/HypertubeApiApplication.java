d469768501a2dbcfbdab454b065ae79c9eeabb22
Henri Sebille <henri.sebille@scub.net>
2025-01-15 10:23:02 +0100

    Initializing project. Basic Maven project using jdk21

---
+++ hypertube-api/src/main/java/com/_2/hypertube_api/HypertubeApiApplication.java
@@ -0,0 +1,13 @@
+package com._2.hypertube_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypertubeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypertubeApiApplication.class, args);
+	}
+
+}
---
+++ hypertube-api/src/test/java/com/_2/hypertube_api/HypertubeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com._2.hypertube_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypertubeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
