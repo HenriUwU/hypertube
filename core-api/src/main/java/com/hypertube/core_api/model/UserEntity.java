d44bb1fa88c477e1f8b778ac86d8fa1e96a696d8
HenriUwU <henrisebille69@gmail.com>
2025-01-15 12:00:46 +0100

    changed UserEntity, probably done.

--- core-api/src/main/java/com/hypertube/core_api/model/UserEntity.java
+++ core-api/src/main/java/com/hypertube/core_api/model/UserEntity.java
@@ -2,37 +2,49 @@ package com.hypertube.core_api.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class UserEntity {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column
+	@Column(nullable = false, unique = true)
 	private String username;
 
-	@Column
+	@Column(nullable = false, unique = true)
 	private String email;
 
-	@Column
+	@Column(nullable = false)
 	private String password;
 
-	@Column
 	private String firstName;
-
-	@Column
 	private String lastName;
+	private String preferredLanguage;
 
-	@Column
 	@Lob
 	private byte[] avatar;
 
-	@Column
-	private String preferredLanguage;
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(nullable = false)
+	private LocalDateTime updatedAt;
 
 }
