e3cb5029b6b333ac97acb16cbb7197e6d70f0a7f
HenriUwU <henrisebille69@gmail.com>
2025-01-23 01:12:36 +0100

    css again on login page

--- hypertube-frontend/src/app/components/login-page/login-page.component.ts
+++ hypertube-frontend/src/app/components/login-page/login-page.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
 }
