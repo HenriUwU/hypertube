ed1ccb92247d583d395525ae0934b06bf4c07582
painpats <cbourrewe@gmail.com>
2025-04-09 15:38:15 +0200

    header in component functionnal

--- hypertube-frontend/src/app/app.component.ts
+++ hypertube-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {GlobalMessageComponent} from "./components/global-message/global-message.component";
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GlobalMessageComponent],
+  imports: [RouterOutlet, GlobalMessageComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
