b082ce9996a5c871de7bc9914e56d1d41c308ce2
painpats <cbourrewe@gmail.com>
2025-04-10 16:04:40 +0200

    language: down, search bar: beginning

--- hypertube-frontend/src/app/app.component.ts
+++ hypertube-frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {GlobalMessageComponent} from "./components/global-message/global-message.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { Ng2SearchPipeModule } from '../../node_modules/';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GlobalMessageComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, GlobalMessageComponent, HeaderComponent, FooterComponent, Ng2SearchPipeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- hypertube-frontend/src/app/components/header/header.component.ts
+++ hypertube-frontend/src/app/components/header/header.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+interface Language {
+  name: string;
+  sound: string;
+}
 
 @Component({
   selector: 'app-header',
-  imports: [],
   standalone: true,
+  imports: [MatDividerModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  languageControl = new FormControl<Language | null>(null, Validators.required);
+  selectFormControl = new FormControl('', Validators.required);
+  languages: Language[] = [
+    {name: 'English', sound: 'Can I get some Burger ?'},
+    {name: 'Francais', sound: 'Oui oui baguette'},
+    {name: 'Spanish', sound: 'Me gustas tus'},
+  ];
+  term: any;
 }
+
+
+
