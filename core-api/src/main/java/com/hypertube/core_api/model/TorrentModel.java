24b894227053dae87410c9132345d27a4e84eff0
HenriUwU <henrisebille69@gmail.com>
2025-04-11 16:45:27 +0200

    added the torrent search logic controller

---
+++ core-api/src/main/java/com/hypertube/core_api/model/TorrentModel.java
@@ -0,0 +1,86 @@
+package com.hypertube.core_api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TorrentModel {
+
+	@JsonProperty("Name")
+	private String name;
+
+	@JsonProperty("Same")
+	private String size;
+
+	@JsonProperty("Category")
+	private String category;
+
+	@JsonProperty("Seeders")
+	private String seeders;
+
+	@JsonProperty("Leechers")
+	private String leechers;
+
+	@JsonProperty("Url")
+	private String url;
+
+	@JsonProperty("Magnet")
+	private String magnet;
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		category = category;
+	}
+
+	public String getLeechers() {
+		return leechers;
+	}
+
+	public void setLeechers(String leechers) {
+		this.leechers = leechers;
+	}
+
+	public String getMagnet() {
+		return magnet;
+	}
+
+	public void setMagnet(String magnet) {
+		this.magnet = magnet;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSeeders() {
+		return seeders;
+	}
+
+	public void setSeeders(String seeders) {
+		this.seeders = seeders;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
