cd236e81d22790fba1f55088b77502b7b816373c
painpats <cbourrewe@gmail.com>
2025-04-11 11:35:24 +0200

    search package: moved in the rigth place, need to access the db

--- hypertube-frontend/src/app/app.component.ts
+++ hypertube-frontend/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import {GlobalMessageComponent} from "./components/global-message/global-message.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { Ng2SearchPipeModule } from '../../node_modules/';
+import { Ng2SearchPipeModule } from '../../node_modules/ng2-search-filter';
 
 @Component({
   selector: 'app-root',
