ee022f83d532a3bfd9a042d40219aa120f688778
nlocusso <nathan.locussol@gmail.com>
2025-06-19 15:59:04 +0200

    fix flag for translate language

--- core-api/src/main/java/com/hypertube/core_api/service/TranslateService.java
+++ core-api/src/main/java/com/hypertube/core_api/service/TranslateService.java
@@ -22,11 +22,59 @@ public class TranslateService {
     private final ObjectMapper objectMapper;
     @Value("${tmdb.bearer-token}")
     private String tmdbToken;
+    private static final Map<String, String> ISO_639_TO_ISO_3166 = Map.ofEntries(
+            Map.entry("en", "gb"),
+            Map.entry("fr", "fr"),
+            Map.entry("de", "de"),
+            Map.entry("es", "es"),
+            Map.entry("it", "it"),
+            Map.entry("pt", "pt"),
+            Map.entry("ru", "ru"),
+            Map.entry("nl", "nl"),
+            Map.entry("sv", "se"),
+            Map.entry("da", "dk"),
+            Map.entry("nb", "no"),
+            Map.entry("fi", "fi"),
+            Map.entry("pl", "pl"),
+            Map.entry("cs", "cz"),
+            Map.entry("sk", "sk"),
+            Map.entry("hu", "hu"),
+            Map.entry("ro", "ro"),
+            Map.entry("bg", "bg"),
+            Map.entry("hr", "hr"),
+            Map.entry("sl", "si"),
+            Map.entry("et", "ee"),
+            Map.entry("lv", "lv"),
+            Map.entry("lt", "lt"),
+            Map.entry("el", "gr"),
+            Map.entry("tr", "tr"),
+            Map.entry("uk", "ua"),
+            Map.entry("ga", "ie"),
+            Map.entry("ca", "es"),
+            Map.entry("eu", "es"),
+            Map.entry("gl", "es"),
+            Map.entry("sq", "al"),
+            Map.entry("ja", "jp"),
+            Map.entry("ko", "kr"),
+            Map.entry("zh", "cn"),
+            Map.entry("hi", "in"),
+            Map.entry("bn", "bd"),
+            Map.entry("ur", "pk"),
+            Map.entry("th", "th"),
+            Map.entry("vi", "vn"),
+            Map.entry("ms", "my"),
+            Map.entry("id", "id"),
+            Map.entry("tl", "ph"),
+            Map.entry("az", "az"),
+            Map.entry("fa", "ir"),
+            Map.entry("he", "il"),
+            Map.entry("ar", "sa"),
+            Map.entry("eo", "un")
+    );
 
     public TranslateService(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
         this.restTemplate = new RestTemplate();
-        // this.translateApiUrl = "http://locahost:5000/";
         this.translateApiUrl = "http://libretranslate:5000/";
     }
 
@@ -89,7 +137,7 @@ public class TranslateService {
             for (LanguageModel languageModel : response.getBody()) {
                 String code = languageModel.getIso_639_1();
                 if (supportedCodes.contains(code)) {
-                    languageModel.setFlag("https://flagcdn.com/w80/" + code + ".png");
+                    languageModel.setFlag("https://flagcdn.com/16x12/" + ISO_639_TO_ISO_3166.get(code.toLowerCase()) + ".png");
                     languageList.add(languageModel);
                 }
             }
@@ -98,4 +146,5 @@ public class TranslateService {
         }
         return new ResponseEntity<>(languageList, HttpStatus.OK);
     }
+
 }
