aa6eaf37880bf1dce2c07569731b798b72ea9c36
Armand Damiens <adamiens@1G3.42angouleme.fr>
2025-06-20 14:33:37 +0200

    Update: profile modify password nutton goes down if it's cellphone view + trait du footer baissé

--- hypertube-frontend/src/app/components/footer/footer.component.ts
+++ hypertube-frontend/src/app/components/footer/footer.component.ts
@@ -1,19 +1,3 @@
-// import { Component } from '@angular/core';
-// import {MatIconModule} from '@angular/material/icon';
-// import {MatDividerModule} from '@angular/material/divider';
-// import {MatButtonModule} from '@angular/material/button';
-
-// @Component({
-//   selector: 'app-footer',
-//   standalone: true,
-//   imports: [MatDividerModule, MatButtonModule, MatIconModule],
-//   templateUrl: './footer.component.html',
-//   styleUrl: './footer.component.css'
-// })
-// export class FooterComponent {
-
-// }
-
 import { Component } from "@angular/core"
 import { CommonModule } from "@angular/common"
 import { MatDividerModule } from "@angular/material/divider"
